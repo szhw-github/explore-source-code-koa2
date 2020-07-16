05ee081c28c3b1d0d90da911394f239016acda4f
szhw <1872651967@qq.com>
2020-07-16 18:54:46 +0800

    write a middleware to package two funciton

---
+++ src/middleware.js
@@ -0,0 +1,23 @@
+
+function add(a, b) {
+    return a + b
+}
+
+function double(c) {
+    return c*2
+}
+
+const middlewares = [add, double]
+
+function compose(midds) {
+    return (...args) => {
+        let res = midds[0](...args)
+        for(let i = 1; i<midds.length; i++) {
+            res = midds[i](res)
+        }
+        return res
+    }
+}
+
+let f = compose(middlewares)
+console.log(f(1,2))
